40bdc49f6d2aed9fefac0c89ccafcec81b925821
hanenWahabi <hanenouahabii@gmail.com>
2025-10-31 10:34:46 +0100

    fix v1

--- android/src/main/java/com/baseflow/location_permissions/LocationPermissionsPlugin.java
+++ android/src/main/java/com/baseflow/location_permissions/LocationPermissionsPlugin.java
@@ -1,10 +1,6 @@
-
 package com.baseflow.location_permissions;
 
-import android.content.Context;
-
 import androidx.annotation.NonNull;
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -13,35 +9,21 @@ import io.flutter.plugin.common.MethodChannel.Result;
 
 public class LocationPermissionsPlugin implements FlutterPlugin, MethodCallHandler {
     private MethodChannel channel;
-    private Context context;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        context = flutterPluginBinding.getApplicationContext();
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "location_permissions");
         channel.setMethodCallHandler(this);
     }
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        switch (call.method) {
-            case "checkPermission":
-                // Implement permission check logic here
-                result.success("Permission check not implemented");
-                break;
-            case "requestPermission":
-                // Implement permission request logic here
-                result.success("Permission request not implemented");
-                break;
-            default:
-                result.notImplemented();
-        }
+        // Implement your permission methods here
+        result.notImplemented();
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
-        channel = null;
-        context = null;
     }
 }
